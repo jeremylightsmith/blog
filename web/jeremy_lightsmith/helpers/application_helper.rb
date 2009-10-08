524c1e7679e8469aa1e7f47a9b5f7af6ab0f2945
Jeremy Lightsmith <jeremy@flave.local>
2009-10-08 16:44:31 -0700

    tracking code and retro descriptions

--- web/jeremy_lightsmith/helpers/application_helper.rb
+++ web/jeremy_lightsmith/helpers/application_helper.rb
@@ -33,4 +33,19 @@ module ApplicationHelper
       </div>
     HTML
   end
+  
+  def tracking_code
+    <<-HTML
+    <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var pageTracker = _gat._getTracker("UA-11025364-1");
+        pageTracker._trackPageview();
+      } catch(err) {}
+    </script>
+    HTML
+  end
 end
