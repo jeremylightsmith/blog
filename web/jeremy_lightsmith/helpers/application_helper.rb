973e4d8ef8c4e2852e613721b146aae1960cecb7
Jeremy Lightsmith <jeremy@flave.local>
2009-10-08 13:56:28 -0700

    make jl.com gorgeous

--- web/jeremy_lightsmith/helpers/application_helper.rb
+++ web/jeremy_lightsmith/helpers/application_helper.rb
@@ -1,27 +1,27 @@
 module ApplicationHelper
-  def sidebar
+  def what_i_offer
     <<-HTML
-    <div class="sidebar">
-      <h2>What I offer</h2>
-      <ul>
-        <li>
-          #{ link_to "Requirements Discovery Workshops", "/services.html#requirements_discovery_workshops" }
-          <div class="hint">build the right product</div>
-        </li>
-        <li>
-          #{ link_to "Agile/Scrum Enablement", "/services.html#agile_enablement" }
-          <div class="hint">deliver it when you said you would</div>
-        </li>
-        <li>
-          #{ link_to "Retrospectives", "/services.html#retrospectives" }
-          <div class="hint">adopt a culture of innovation</div>
-        </li>
-        <li>
-          #{ link_to "Developer Best Practice Workshops", "/services.html#best_practices" }
-          <div class="hint">make it all fun</div>
-        </li>
-      </ul>
-    </div>
+      <div class="box">
+        <h2>What I offer</h2>
+        <ul>
+          <li>
+            #{ link_to "Requirements Discovery Workshops", "/services.html#requirements_discovery_workshops" }
+            <div class="hint">build the right product</div>
+          </li>
+          <li>
+            #{ link_to "Agile/Scrum Enablement", "/services.html#agile_enablement" }
+            <div class="hint">deliver it when you said you would</div>
+          </li>
+          <li>
+            #{ link_to "Retrospectives", "/services.html#retrospectives" }
+            <div class="hint">adopt a culture of innovation</div>
+          </li>
+          <li>
+            #{ link_to "Developer Best Practice Workshops", "/services.html#best_practices" }
+            <div class="hint">make it all fun</div>
+          </li>
+        </ul>
+      </div>
     HTML
   end
 end
